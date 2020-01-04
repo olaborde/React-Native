17671e0adc638748c31bea7480ed307fb9d89196
olaborde <oml007@live.com>
2020-01-04 15:19:41 -0500

    added drawer navigation

--- nucampsite/components/MainComponents.js
+++ nucampsite/components/MainComponents.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
+import Home from './HomeComponent'
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -23,6 +24,33 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
+const HomeNavigator = createStackNavigator(
+    {
+        Home: {screen: Home}
+    },
+    {
+        
+        navigationOptions: {
+            headerStyle:  {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: {screen: HomeNavigator},
+        Directory: {screen: DirectoryNavigator}
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+)
 
 class Main extends Component{
 
@@ -34,7 +62,7 @@ class Main extends Component{
                 flex: 1,
                 paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constant.statusBarHeight
             }}>
-                <DirectoryNavigator />
+                <MainNavigator />
             </View>
         );
     }
